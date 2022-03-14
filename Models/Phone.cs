db64303897eab5a24bdc0a85d901f73b9fe61f5f
Aysel <tu7pgums7@code.edu.az>
2022-03-14 06:46:45 +0400

    Proje dosyası ekle.

---
+++ Models/Phone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace AztuTask
+{  
+
+internal class Phone
+{
+    public string name;
+    public string producer;
+    public int year;
+    public string version;
+    public string color;
+    public string ram;
+
+    public Phone(string name, string producer, int year)
+    { 
+        this.name = name;
+        this.producer = producer;
+        this.year = year;
+              
+    }
+
+
+    public Phone(string name, string producer,int year, string version, string color) : this (name, producer, year)
+    {
+        this.version = version;
+        this.color = color;
+    }
+
+    public Phone(string name, string producer,int year, string version, string color, string ram): this (name,producer, year, version,color)
+    {
+        this.ram = ram;
+    }
+  }
+}
---
+++ program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using AztuTask;
+using AztuTask.Models;
+
+
+
+
+
+namespace System
+{
+  internal class Program
+  {
+        static void Main(string[] args)
+      {
+        #region objekt
+       
+        Phone phone = new Phone("S21fe", "Samsung", "Android", "2021", "Red", "8gb");
+
+            Console.WriteLine("Phone.name");
+                       
+
+                      #endregion
+
+        }     
+   }          
+}
+  
+
+
+
